91c5909b0020c7b20b79d13559dfd5ff24d4051f
SeltikeS <pschenka@gmail.com>
2022-05-05 19:01:01 +0300

    extra: add functions for 2nd extra

--- task2/main.js
+++ task2/main.js
@@ -1,6 +1,3 @@
-// const map = { key: "value" };
-// const indexedMap = { 1: "key" };
-
 class IndexedMap {
   constructor() {
     this._map = {};
@@ -9,10 +6,33 @@ class IndexedMap {
     this._counter = 0;
   }
 
+  print() {
+    const indexedArray = [];
+    for (let index in this._indexed) {
+      const element = {
+        index: index,
+        key: this._indexed[index],
+        value: this._map[this._indexed[index]],
+      };
+      indexedArray.push(element);
+    }
+    return indexedArray;
+  }
+
+  updateIndex() {
+    this._counter = 0;
+    const newIndexed = {};
+    for (let index in this._indexed) {
+      newIndexed[this._counter++] = this._indexed[index];
+    }
+    this._indexed = newIndexed;
+    return this._indexed;
+  }
+
   set(key, value) {
     if (key && value) {
       if (!this._map[key]) {
-        this._indexed[++this._counter] = key;
+        this._indexed[this._counter++] = key;
         this._size++;
       }
       this._map[key] = value;
@@ -55,6 +75,7 @@ class IndexedMap {
         if (this._indexed[idx] === key) {
           delete this._indexed[idx];
           --this._size;
+          this.updateIndex();
           break;
         }
       }
@@ -62,17 +83,55 @@ class IndexedMap {
     return this.print();
   }
 
-  print() {
-    const indexedArray = [];
+  forEach(fn) {
     for (let index in this._indexed) {
-      const element = {
-        index: index,
-        key: this._indexed[index],
-        value: this._map[this._indexed[index]],
-      };
-      indexedArray.push(element);
+      fn(this._map[this._indexed[index]], this._indexed[index], index);
     }
-    return indexedArray;
+    return this.print();
+  }
+
+  union(...maps) {
+    for (let map of maps) {
+      map.forEach((value, key) => {
+        this.set(key, value);
+      });
+    }
+    return this.print();
+  }
+
+  unique() {
+    const newUniqueObject = {};
+    const newUniqueCollection = [];
+    for (let value in this._map) {
+      if (!newUniqueObject[this._map[value]]) {
+        newUniqueObject[this._map[value]] = 1;
+        newUniqueCollection.push(this._map[value]);
+      }
+    }
+    return newUniqueCollection;
+  }
+
+  uniqueKeys() {
+    const newUniqueKeys = [];
+    for (let key in this._map) {
+      newUniqueKeys.push(key);
+    }
+    return newUniqueKeys;
+  }
+
+  removeAt(index, count = 1) {
+    if (index) {
+      for (let i = 1; i <= count; ++i) {
+        if (this._indexed[index + i]) {
+          delete this._map[this._indexed[index + i]];
+          delete this._indexed[index + i];
+        } else {
+          break;
+        }
+      }
+      this.updateIndex();
+    }
+    return this.print();
   }
 
   get size() {
